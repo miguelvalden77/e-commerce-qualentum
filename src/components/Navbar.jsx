928b28dec2e73c16154a7fa58524b7928bcdec24
miguelvalden <miguelvalden77@gmail.com>
2024-03-21 00:18:44 +0100

    lab 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom"
+
+const rutas = [
+    { nombre: "Inicio", link: "/" },
+    { nombre: "Categorías", link: "/categorias" },
+    { nombre: "Ofertas", link: "/ofertas" },
+    { nombre: "Contacto", link: "/contacto" }
+]
+
 const Navbar = () => {
 
     return (
-        <nav>
-
+        <nav className="navbar">
+            <section className="link-section">
+                {
+                    rutas.map(({ nombre, link }) => (
+                        <Link key={nombre} to={link}>{nombre}</Link>
+                    ))
+                }
+            </section>
+            <input className="input-search" type="text" placeholder="Buscar productos" />
+            <section className="emojis-section">
+                <Link to={"#"}>🛒</Link>
+                <Link to={"#"}>💙</Link>
+                <Link to={"#"}>🧔</Link>
+            </section>
         </nav>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import data from "../../data.json"
+
 const Home = () => {
     return (
-        <main>
-            <h1>Home</h1>
+        <main className="main-products">
+            {
+                data.map(({ id, title, price, image, description }) => (
+                    <article className="product-card" key={id}>
+                        <div className="img-container">
+                            <img src={image} alt={title} />
+                        </div>
+                        <div className="info-container">
+                            <h2>{title}</h2>
+                            <p>{description}</p>
+                            <p className="price">{price}$</p>
+                        </div>
+                    </article>
+                ))
+            }
         </main>
     )
 }
